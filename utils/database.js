0f8b6641f70549e5b74a4c980568bd368d081dbe
hungvo2010 <hungvochanhdp1@gmail.com>
2022-01-19 22:58:12 +0700

    connect via url

--- utils/database.js
+++ utils/database.js
@@ -2,22 +2,22 @@ const Sequelize = require('sequelize');
 const path = require('path');
 require('dotenv').config({path: path.join(__dirname, '../','.env')});
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASS, {
-    dialect: 'mysql',
-    host: process.env.DB_HOST,
-    port: process.env.DB_POST,
-}, {
-    define: {
-       freezeTableName: true, 
-    }
-});
-
-// const sequelize = new Sequelize(process.env.DB_PRODUCTION, {
+// const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASS, {
 //     dialect: 'mysql',
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_POST,
 // }, {
 //     define: {
 //        freezeTableName: true, 
 //     }
 // });
+
+const sequelize = new Sequelize(process.env.DB_URL, {
+    dialect: 'mysql',
+}, {
+    define: {
+       freezeTableName: true, 
+    }
+});
 
 module.exports = sequelize;
