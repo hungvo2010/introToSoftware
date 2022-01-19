7dc45af7c43934ca8b506a2965316973e0582067
hungvo2010 <hungvochanhdp1@gmail.com>
2022-01-19 20:14:17 +0700

    change db configuration

--- utils/database.js
+++ utils/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const path = require('path');
 require('dotenv').config({path: path.join(__dirname, '../','.env')});
 
-const sequelize = new Sequelize('volunteer', process.env.DB_USER, process.env.DB_PASS, {
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASS, {
     dialect: 'mysql',
     host: process.env.DB_HOST,
 }, {
