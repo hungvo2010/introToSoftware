e76ad341513ab263731d0a8599b64f365f20d8e7
hungvo2010 <hungvochanhdp1@gmail.com>
2022-01-20 00:40:47 +0700

    add production config

--- utils/database.js
+++ utils/database.js
@@ -1,23 +1,17 @@
-const Sequelize = require('sequelize');
+'use strict';
+
 const path = require('path');
-require('dotenv').config({path: path.join(__dirname, '../','.env')});
+const Sequelize = require('sequelize');
+require('dotenv').config({path: path.join(__dirname, '../', '.env')});
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
 
-// const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASS, {
-//     dialect: 'mysql',
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_POST,
-// }, {
-//     define: {
-//        freezeTableName: true, 
-//     }
-// });
-
-const sequelize = new Sequelize(process.env.DB_URL, {
-    dialect: 'mysql',
-}, {
-    define: {
-       freezeTableName: true, 
-    }
-});
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 module.exports = sequelize;
