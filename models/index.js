f77becc8220833cacdc06c57e6d79c0cd441772f
hungvo2010 <hungvochanhdp1@gmail.com>
2022-01-19 21:48:30 +0700

    migrate data

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+require('dotenv').config({path: path.join(__dirname, '.env')});
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
