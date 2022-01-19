9cbda9e1a59aa970292ef9ee0c01bd0d97e76d1a
hungvo2010 <hungvochanhdp1@gmail.com>
2022-01-19 20:52:32 +0700

    explictly config sequelize

--- utils/database.js
+++ utils/database.js
@@ -2,8 +2,9 @@ const Sequelize = require('sequelize');
 const path = require('path');
 require('dotenv').config({path: path.join(__dirname, '../','.env')});
 
-const sequelize = new Sequelize(process.env.DB_MYSQL_PRODUCTION, {
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASS, {
     dialect: 'mysql',
+    port: process.env.DB_PORT,
 }, {
     define: {
        freezeTableName: true, 
