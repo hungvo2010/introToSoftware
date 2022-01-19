6925f3c9701b729acbab8a886a1868c1f66dd1bd
hungvo2010 <hungvochanhdp1@gmail.com>
2022-01-19 22:33:32 +0700

    revert commit to deploy succcess first

--- utils/database.js
+++ utils/database.js
@@ -2,9 +2,9 @@ const Sequelize = require('sequelize');
 const path = require('path');
 require('dotenv').config({path: path.join(__dirname, '../','.env')});
 
-const sequelize = new Sequelize('Volunteer', process.env.DB_USER, process.env.DB_PASS, {
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASS, {
     dialect: 'mysql',
-    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
 }, {
     define: {
        freezeTableName: true, 
