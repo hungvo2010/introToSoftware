69efdb512194e4d75588060f8c36f22899deeb28
hungvo2010 <hungvochanhdp1@gmail.com>
2022-01-19 22:32:39 +0700

    testing

--- utils/database.js
+++ utils/database.js
@@ -2,21 +2,21 @@ const Sequelize = require('sequelize');
 const path = require('path');
 require('dotenv').config({path: path.join(__dirname, '../','.env')});
 
-// const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASS, {
-//     dialect: 'mysql',
-//     port: process.env.DB_PORT,
-// }, {
-//     define: {
-//        freezeTableName: true, 
-//     }
-// });
-
-const sequelize = new Sequelize(process.env.DB_PRODUCTION, {
+const sequelize = new Sequelize('Volunteer', process.env.DB_USER, process.env.DB_PASS, {
     dialect: 'mysql',
+    port: process.env.DB_PORT,
 }, {
     define: {
        freezeTableName: true, 
     }
 });
+
+// const sequelize = new Sequelize(process.env.DB_PRODUCTION, {
+//     dialect: 'mysql',
+// }, {
+//     define: {
+//        freezeTableName: true, 
+//     }
+// });
 
 module.exports = sequelize;
