1be88244e2b85ffa14aa7e9025aa148d56aa57a8
hungvo2010 <hungvochanhdp1@gmail.com>
2022-01-19 20:29:39 +0700

    reconfiguration sequelize connect database

--- utils/database.js
+++ utils/database.js
@@ -2,9 +2,8 @@ const Sequelize = require('sequelize');
 const path = require('path');
 require('dotenv').config({path: path.join(__dirname, '../','.env')});
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASS, {
+const sequelize = new Sequelize(process.env.DB_MYSQL_PRODUCTION, {
     dialect: 'mysql',
-    host: process.env.DB_HOST,
 }, {
     define: {
        freezeTableName: true, 
