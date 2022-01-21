d53665fcd8e147dd9fc306f7d0fea97fcb002c1a
hungvo2010 <hungvochanhdp1@gmail.com>
2022-01-21 20:10:57 +0700

    force delete table

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.use(function(err, req, res, next) {
   // res.render('error');
 });
 
-sequelize.sync()
+sequelize.sync({force: true})
 .then(db => {
   app.listen(process.env.PORT || 3000);
 })
